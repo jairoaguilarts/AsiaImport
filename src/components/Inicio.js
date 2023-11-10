269901c80dec85572f1b5e54eaed9dc91d1e716a
Julio Salgado <jyahirsalgado@gmail.com>
2023-11-10 10:58:04 -0600

    Adicion de la seccion de productos destacados

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -10,7 +10,65 @@ import botestermosImage from '../assets/botestermos.png';
 import cargadoresImage from '../assets/cargadores.png';
 import relojesImage from '../assets/relojes.png';
 import otrosImage from '../assets/otros.png';
+import parlanteProduct from '../assets/Parlante Bluetooth.png';
+import radio from '../assets/Radio Recargable.png';
+import audifonosProduct from '../assets/Audifonos UltraSound.png';
+import yeti from '../assets/Yeti Fake.png';
 import './Inicio.css';
+
+const productos = [
+  { imagen: parlanteProduct, nombre: 'Parlante Bluetooth con Luz Led', precio: 'L. 849' },
+  { imagen: radio, nombre: 'Radio Recargable con Panel Solar', precio: 'L. 499' },
+  { imagen: audifonosProduct, nombre: 'Audifonos UltraSound inalambricos', precio: 'L. 2,449' },
+  { imagen: yeti, nombre: 'Vasos Termicos Yeti', precio: 'L. 349' }
+  
+];
+
+const Carrusel = ({ productos }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const itemsVisibles = 4;
+
+  const mostrarProductos = productos.length < itemsVisibles
+    ? productos
+    : productos.concat(productos).slice(currentIndex, currentIndex + itemsVisibles);
+
+  const moverCarrusel = (direccion) => {
+      const totalItems = productos.length;
+      let newIndex = (currentIndex + direccion + totalItems) % totalItems;
+
+      setCurrentIndex(newIndex);
+  };
+
+  const handlePrevClick = (e) => {
+    e.preventDefault();
+    moverCarrusel(-1);
+  };
+
+  const handleNextClick = (e) => {
+    e.preventDefault();
+    moverCarrusel(1);
+  };
+
+  return (
+      <div className="destacado-container">
+        <div className="destacado-product-container">
+          <button className="prev-btn" onClick={handlePrevClick}>&#9664;</button>
+          <div className="destacado-lista">
+              {mostrarProductos.map((producto, index) => (
+                  <div key={index} className="elemento">
+                      <img src={producto.imagen} alt={producto.nombre} />
+                      <h3>{producto.nombre}</h3>
+                      <p>Precio: {producto.precio}</p>
+                      <button>Agregar al carrito</button>
+                  </div>
+              ))}
+          </div>
+          <button className="next-btn" onClick={handleNextClick}>&#9654;</button>
+        </div>
+      </div>
+  );
+};
 
 function Inicio() {
     const [index, setIndex] = useState(0);
@@ -29,6 +87,12 @@ function Inicio() {
         className="d-block w-100" src={PromocionAsia}  alt="First slide"/>
       </Carousel.Item>
     </Carousel>
+    <div className="section-divider">
+      <p className="destacado-text"> Articulos Destacados </p>
+      <hr className="linea-divisora-blue-large" />
+      <Carrusel productos={productos} />
+      <hr className="linea-divisora-blue-med" />
+    </div>
     <div className="product-section">
         <div className="product-container">
           <a href="/parlantes" className="product-item">
