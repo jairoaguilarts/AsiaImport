56a8b58f857dde42cb3949d278cd7f9c3b629a56
jairoaguilarts <jairoaguilar1219@gmail.com>
2023-11-10 07:27:38 -0600

    Cambiando de componente las categorias

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,12 +4,6 @@ import { Link } from 'react-router-dom';
 import './Footer.css';
 
 // Importa las imágenes desde tu carpeta assets
-import parlantesImage from '../assets/parlantes.png';
-import audifonosImage from '../assets/audifonos.png';
-import botestermosImage from '../assets/botestermos.png';
-import cargadoresImage from '../assets/cargadores.png';
-import relojesImage from '../assets/relojes.png';
-import otrosImage from '../assets/otros.png';
 import facebookIcon from '../assets/facebook.png';
 import instagramIcon from '../assets/instagram.png';
 import logoImage from '../assets/LogoPng.png';
@@ -20,34 +14,6 @@ const Footer = () => {
 
   return (
     <footer className="footer">
-      <div className="product-section">
-        <div className="product-container">
-          <a href="/parlantes" className="product-item">
-            <img src={parlantesImage} alt="Parlantes" />
-            <p>Parlantes</p>
-          </a>
-          <a href="/audifonos" className="product-item">
-            <img src={audifonosImage} alt="Audífonos" />
-            <p>Audífonos</p>
-          </a>
-          <a href="/botestermos" className="product-item">
-            <img src={botestermosImage} alt="Botes y Termos" />
-            <p>Botes y Termos</p>
-          </a>
-          <a href="/cargadores" className="product-item">
-            <img src={cargadoresImage} alt="Cargadores para móvil" />
-            <p>Cargadores para móvil</p>
-          </a>
-          <a href="/relojesinteligentes" className="product-item">
-            <img src={relojesImage} alt="Relojes Inteligentes" />
-            <p>Relojes Inteligentes</p>
-          </a>
-          <a href="/otros" className="product-item">
-            <img src={otrosImage} alt="Otros" />
-            <p>Otros</p>
-          </a>
-        </div>
-      </div>
       <div className="footer-content">
         <div className="social-media">
           <p className="social-media-label">Redes Sociales</p>
--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -4,6 +4,12 @@ import CarouselItem from 'react-bootstrap/CarouselItem'
 import asiaImport from '../imagenes/asiaImport.png';
 import PromoAsia from '../imagenes/PromoAsia.jpeg';
 import PromocionAsia from '../imagenes/PromocionAsia.jpg';
+import parlantesImage from '../assets/parlantes.png';
+import audifonosImage from '../assets/audifonos.png';
+import botestermosImage from '../assets/botestermos.png';
+import cargadoresImage from '../assets/cargadores.png';
+import relojesImage from '../assets/relojes.png';
+import otrosImage from '../assets/otros.png';
 import './Inicio.css';
 
 function Inicio() {
@@ -23,6 +29,34 @@ function Inicio() {
         className="d-block w-100" src={PromocionAsia}  alt="First slide"/>
       </Carousel.Item>
     </Carousel>
+    <div className="product-section">
+        <div className="product-container">
+          <a href="/parlantes" className="product-item">
+            <img src={parlantesImage} alt="Parlantes" />
+            <p>Parlantes</p>
+          </a>
+          <a href="/audifonos" className="product-item">
+            <img src={audifonosImage} alt="Audífonos" />
+            <p>Audífonos</p>
+          </a>
+          <a href="/botestermos" className="product-item">
+            <img src={botestermosImage} alt="Botes y Termos" />
+            <p>Botes y Termos</p>
+          </a>
+          <a href="/cargadores" className="product-item">
+            <img src={cargadoresImage} alt="Cargadores para móvil" />
+            <p>Cargadores para móvil</p>
+          </a>
+          <a href="/relojesinteligentes" className="product-item">
+            <img src={relojesImage} alt="Relojes Inteligentes" />
+            <p>Relojes Inteligentes</p>
+          </a>
+          <a href="/otros" className="product-item">
+            <img src={otrosImage} alt="Otros" />
+            <p>Otros</p>
+          </a>
+        </div>
+      </div>
    </div>
   );
 }
