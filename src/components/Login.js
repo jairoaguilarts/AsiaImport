f3520ff4a99aaabc2545761cc1379850b9afa666
EdgarMembreno <edgar.membreno2014@gmail.com>
2023-11-14 14:41:09 -0600

    Formulario Registrarse
    Terminado

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,76 +1,100 @@
-import './Login.css';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 import userIcon from '../assets/user.png';
+import './Login.css';
 
 function Login() {
   const [show, setShow] = useState(false);
-  const [iniciarSesionSeleccionado, setIniciarSesionSeleccionado] = useState(true);
-  const [registrarseSeleccionado, setRegistrarseSeleccionado] = useState(false);
+  const [isLoginSelected, setIsLoginSelected] = useState(true);
 
-  const handleIniciarSesionClick = () => {
-    setIniciarSesionSeleccionado(true);
-    setRegistrarseSeleccionado(false);
-  };
-  
-  const handleRegistrarseClick = () => {
-    setIniciarSesionSeleccionado(false);
-    setRegistrarseSeleccionado(true);
-  };
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+  const handleIniciarSesionClick = () => {
+    setIsLoginSelected(true);
+  };
+
+  const handleRegistrarseClick = () => {
+    setIsLoginSelected(false);
+  };
+
   return (
     <>
-     <button onClick={handleShow} className="icon-button" >
-            <img src={userIcon} alt="User" className="icon" />
-            <p>Iniciar Sesion</p>
-          </button>
+      <button onClick={handleShow} className="icon-button" >
+        <img src={userIcon} alt="User" className="icon" />
+        <p>Iniciar Sesión</p>
+      </button>
 
-      <Modal show={show} onHide={handleClose} size="md"  centered>
+      <Modal show={show} onHide={handleClose} size="md" centered>
         <Modal.Header closeButton>
           <Modal.Title>
             <div className="button-container">
-                <button  onClick={handleIniciarSesionClick}  className={`iniciar-sesion ${iniciarSesionSeleccionado ? 'selected' : ''}`}>
-                <p>Iniciar Sesion</p>
-                </button>
-                <button onClick={handleRegistrarseClick} className={`registrarse ${registrarseSeleccionado ? 'selected' : ''}`}>
+              <button onClick={handleIniciarSesionClick} className={`iniciar-sesion ${isLoginSelected ? 'selected' : ''}`}>
+                <p>Iniciar Sesión</p>
+              </button>
+              <button onClick={handleRegistrarseClick} className={`registrarse ${!isLoginSelected ? 'selected' : ''}`}>
                 <p>Registrarse</p>
-                </button>
-          </div>
+              </button>
+            </div>
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <Form>
-            <Form.Group className="forms" controlId="exampleForm.ControlInput1">
-              <Form.Label>Correo</Form.Label>
-              <Form.Control
-                type="email"
-                autoFocus
-              />
-            </Form.Group>
-            <Form.Group className="forms" controlId="exampleForm.ControlInput1">
-              <Form.Label>Contraseña</Form.Label>
-              <Form.Control
-                type="password"
-                autoFocus
-              />
-            </Form.Group>
+            {isLoginSelected ? (
+              // Formulario de inicio de sesión
+              <>
+                <Form.Group className="forms" controlId="formBasicEmail">
+                  <Form.Label>Correo</Form.Label>
+                  <Form.Control type="email" placeholder="Correo Electrónico" autoFocus />
+                </Form.Group>
+                <Form.Group className="forms" controlId="formBasicPassword">
+                  <Form.Label>Contraseña</Form.Label>
+                  <Form.Control type="password" placeholder="Contraseña" autoFocus />
+                </Form.Group>
+              </>
+            ) : (
+              // Formulario de registro con campos adicionales
+              <>
+                <Form.Group className="forms" controlId="formBasicNombre">
+                  <Form.Label>Nombre</Form.Label>
+                  <Form.Control type="text" placeholder="Nombre" />
+                </Form.Group>
+                <Form.Group className="forms" controlId="formBasicApellido">
+                  <Form.Label>Apellido</Form.Label>
+                  <Form.Control type="text" placeholder="Apellido" />
+                </Form.Group>
+                <Form.Group className="forms" controlId="formBasicID">
+                  <Form.Label>ID O RTN</Form.Label>
+                  <Form.Control type="text" placeholder="ID o RTN" />
+                </Form.Group>
+                <Form.Group className="forms" controlId="formBasicEmailRegistro">
+                  <Form.Label>Correo Electrónico</Form.Label>
+                  <Form.Control type="email" placeholder="Correo Electrónico" />
+                </Form.Group>
+                <Form.Group className="forms" controlId="formBasicPasswordRegistro">
+                  <Form.Label>Contraseña</Form.Label>
+                  <Form.Control type="password" placeholder="Contraseña" />
+                </Form.Group>
+                <Form.Group className="forms" controlId="formBasicConfirmPassword">
+                  <Form.Label>Confirmar Contraseña</Form.Label>
+                  <Form.Control type="password" placeholder="Confirmar Contraseña" />
+                </Form.Group>
+              </>
+            )}
           </Form>
-          <div className="d-flex justify-content-center ">
+          <div className={`d-flex justify-content-center ${isLoginSelected ? '' : 'hidden'}`}>
             <button className="forgot" onClick={handleClose}>
-                ¿Olvidaste tu Contraseña?
+              ¿Olvidaste tu Contraseña?
             </button>
-        </div>
-        <div className="d-flex justify-content-center ">
+          </div>
+          <div className="d-flex justify-content-center">
             <Button className="login" variant="primary" size="lg" onClick={handleClose}>
-                INICIAR SESION
+              {isLoginSelected ? 'INICIAR SESIÓN' : 'REGISTRARSE'}
             </Button>
-        </div>
+          </div>
         </Modal.Body>
-        
       </Modal>
     </>
   );
