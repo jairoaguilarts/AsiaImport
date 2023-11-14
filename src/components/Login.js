8bf7c86d8ae1d483353aaf353f3d9be3f1fa9562
Julio Salgado <jyahirsalgado@gmail.com>
2023-11-14 16:39:37 -0600

    Creacion de la ventana para recuperar la contra

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,13 +3,19 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 import userIcon from '../assets/user.png';
+import iconoLock from '../assets/lock.png'
 import './Login.css';
 
 function Login() {
   const [show, setShow] = useState(false);
   const [isLoginSelected, setIsLoginSelected] = useState(true);
+  const [showVentanaForgot, setShowVentanaForgot] = useState(false);
 
-  const handleClose = () => setShow(false);
+  const handleClose = () => {
+    setShow(false);
+    setShowVentanaForgot(false);
+  }
+
   const handleShow = () => setShow(true);
 
   const handleIniciarSesionClick = () => {
@@ -20,12 +26,52 @@ function Login() {
     setIsLoginSelected(false);
   };
 
+  const handleForgotShow = () => {
+    setShowVentanaForgot(true);
+    setShow(false);
+  }
+  const handleForgotClose = () => {
+    setShowVentanaForgot(false);
+    setShow(true);
+  }
+
   return (
     <>
       <button onClick={handleShow} className="icon-button" >
         <img src={userIcon} alt="User" className="icon" />
         <p>Iniciar Sesión</p>
       </button>
+      
+      {showVentanaForgot && (
+        // Ventana de Recuperacion de Contraseña
+        <Modal show={showVentanaForgot} onHide={handleForgotClose} size='md' centered>
+          <Modal.Header closeButton>
+            <Modal.Title>
+              <div className='forgot_container'>
+                <img src={iconoLock} alt='icono_lock' className='forgot_icon'/>
+                <h1 className='forgot_titulo'>Recuperar Contraseña</h1>
+              </div>
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p className='body_text'>Ingresa los datos solicitados para generar una nueva contraseña</p>
+            <Form>
+              <Form.Group controlId='exampleForm.ControlInput1'>
+                <Form.Label>Contraseña</Form.Label>
+                <Form.Control
+                  type="password"
+                  autoFocus
+                />
+              </Form.Group>
+            </Form>
+            <div className="d-flex justify-content-center ">
+              <Button className="recuperar" variant="primary" size="md" onClick={handleClose}>
+                RECUPERAR
+              </Button>
+            </div>
+          </Modal.Body>
+        </Modal>
+      )}
 
       <Modal show={show} onHide={handleClose} size="md" centered>
         <Modal.Header closeButton>
@@ -85,7 +131,7 @@ function Login() {
             )}
           </Form>
           <div className={`d-flex justify-content-center ${isLoginSelected ? '' : 'hidden'}`}>
-            <button className="forgot" onClick={handleClose}>
+            <button className="forgot" onClick={handleForgotShow}>
               ¿Olvidaste tu Contraseña?
             </button>
           </div>
