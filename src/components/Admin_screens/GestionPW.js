06837705cd2bd9d1795df6ff8c3417360146670f
EdgarMembreno <edgar.membreno2014@gmail.com>
2023-11-30 14:53:40 -0600

    division de secciones en editar pagina
    divisiones

--- src/components/Admin_screens/GestionPW.js
+++ src/components/Admin_screens/GestionPW.js
@@ -1,12 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './GestionPW.css';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
-import iconEdit from "../../assets/edit .png"; // Replace with the correct path to the edit icon image
-import iconDelete from "../../assets/delete.png";// Replace with the correct path to the delete icon image
-// import productImage1 from './product-image1.png'; // Replace with the correct path to product image
-// import productImage2 from './product-image2.png'; // Replace with the correct path to product image
-// import productImage3 from './product-image3.png'; // Replace with the correct path to product image
+import iconEdit from "../../assets/edit .png";
+import iconDelete from "../../assets/delete.png";
 
 const products = [
     {
@@ -38,92 +35,128 @@ const products = [
 const GestionPW = () => {
     const [showEliminarConfirmar, setShowEliminarConfirmar] = useState(false);
     const [productToDelete, setProductToDelete] = useState(null);
-  
+
+    const nuestrosProductosRef = useRef(null);
+    const editarProductosDestacadosRef = useRef(null);
+    const editarImagenesCarrouselRef = useRef(null);
+    const editarInformacionRef = useRef(null);
+
     const handleShowEliminarConfirmar = (productId) => {
-      setProductToDelete(productId);
-      setShowEliminarConfirmar(true);
+        setProductToDelete(productId);
+        setShowEliminarConfirmar(true);
     };
-  
+
     const handleCloseEliminarConfirmar = () => {
-      setShowEliminarConfirmar(false);
+        setShowEliminarConfirmar(false);
     };
-  
+
     const handleEliminar = () => {
-      // Here you would handle the actual deletion of the product
-      console.log('Deleting product with id:', productToDelete);
-      // After deletion logic is implemented, close the modal
-      setShowEliminarConfirmar(false);
+        console.log('Deleting product with id:', productToDelete);
+        setShowEliminarConfirmar(false);
     };
-  
+
+    const scrollToSection = (ref) => {
+        window.scrollTo({ top: ref.current.offsetTop, behavior: 'smooth' });
+    };
+
     return (
-      <div className="gestion-wrapper">
-        <div className="title-bar">
-          {/* Empty title bar used as a fixed header */}
-        </div>
-        <h1 className="title">Nuestros Productos</h1>
-        <div className="gestion-container">
-          <div className="top-bar">
-            <button className="add-product-btn">Crear Nuevo Producto</button>
-            <div className="search-container">
-              <input type="text" placeholder="Buscar..." className="search-bar" />
+        <div className="gestion-wrapper">
+            {/* Barra de navegación */}
+            <div className="navbar">
+                <button onClick={() => scrollToSection(nuestrosProductosRef)}>Nuestros Productos</button>
+                <button onClick={() => scrollToSection(editarProductosDestacadosRef)}>Editar Productos Destacados</button>
+                <button onClick={() => scrollToSection(editarImagenesCarrouselRef)}>Editar Imágenes Carrousel</button>
+                <button onClick={() => scrollToSection(editarInformacionRef)}>Editar Información</button>
             </div>
-          </div>
-          <div className="product-table">
-            <div className="product-row header">
-              <span>Modelo</span>
-              <span>Categoría</span>
-              <span>Nombre</span>
-              <span>Descripción</span>
-              <span>Imagen</span>
-              <span>Editar</span>
-            </div>
-            {products.map(product => (
-              <div className="product-row" key={product.id}>
-                <span className="product-model">{product.model}</span>
-                <span className="product-category">{product.category}</span>
-                <span className="product-name">{product.name}</span>
-                <span className="product-description">{product.description}</span>
-                {/* Replace 'product.image' with the actual image path */}
-                <img src={product.image} alt="Product" className="product-image" />
-                <div className="product-actions">
-                  <button className="edit-btn" aria-label="Edit">
-                    <img src={iconEdit} alt="Edit" />
-                  </button>
-                  <button 
-                    className="delete-btn" 
-                    aria-label="Delete"
-                    onClick={() => handleShowEliminarConfirmar(product.id)}
-                  >
-                    <img src={iconDelete} alt="Delete" />
-                  </button>
+
+            {/* Sección Nuestros Productos */}
+            <div ref={nuestrosProductosRef} className="section">
+                <div className="title-bar">
+                    {/* Empty title bar used as a fixed header */}
                 </div>
-              </div>
-            ))}
-          </div>
+                <h1 className="title">Nuestros Productos</h1>
+                <div className="gestion-container">
+                    <div className="top-bar">
+                        <button className="add-product-btn">Crear Nuevo Producto</button>
+                        <div className="search-container">
+                            <input type="text" placeholder="Buscar..." className="search-bar" />
+                        </div>
+                    </div>
+                    <div className="product-table">
+                        <div className="product-row header">
+                            <span>Modelo</span>
+                            <span>Categoría</span>
+                            <span>Nombre</span>
+                            <span>Descripción</span>
+                            <span>Imagen</span>
+                            <span>Editar</span>
+                        </div>
+                        {products.map(product => (
+                            <div className="product-row" key={product.id}>
+                                <span className="product-model">{product.model}</span>
+                                <span className="product-category">{product.category}</span>
+                                <span className="product-name">{product.name}</span>
+                                <span className="product-description">{product.description}</span>
+                                <img src={product.image} alt="Product" className="product-image" />
+                                <div className="product-actions">
+                                    <button className="edit-btn" aria-label="Edit">
+                                        <img src={iconEdit} alt="Edit" />
+                                    </button>
+                                    <button 
+                                        className="delete-btn" 
+                                        aria-label="Delete"
+                                        onClick={() => handleShowEliminarConfirmar(product.id)}
+                                    >
+                                        <img src={iconDelete} alt="Delete" />
+                                    </button>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+
+            {/* Sección Editar Productos Destacados */}
+            <div ref={editarProductosDestacadosRef} className="section">
+                <h2>Editar Productos Destacados</h2>
+                {/* Contenido para Editar Productos Destacados */}
+            </div>
+
+            {/* Sección Editar Imágenes Carrousel */}
+            <div ref={editarImagenesCarrouselRef} className="section">
+                <h2>Editar Imágenes Carrousel</h2>
+                {/* Contenido para Editar Imágenes Carrousel */}
+            </div>
+
+            {/* Sección Editar Información */}
+            <div ref={editarInformacionRef} className="section">
+                <h2>Editar Información</h2>
+                {/* Contenido para Editar Información */}
+            </div>
+
+            {/* Modal para confirmación de eliminación */}
+            <Modal
+                className="modal-confirmar"
+                show={showEliminarConfirmar}
+                onHide={handleCloseEliminarConfirmar}
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>Confirmar Eliminación</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    ¿Estás seguro de que deseas eliminar este producto?
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleCloseEliminarConfirmar}>
+                        Cancelar
+                    </Button>
+                    <Button variant="danger" onClick={handleEliminar}>
+                        Eliminar
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </div>
-        {/* Modal for delete confirmation */}
-        <Modal
-          className="modal-confirmar"
-          show={showEliminarConfirmar}
-          onHide={handleCloseEliminarConfirmar}
-        >
-          <Modal.Header closeButton>
-            <Modal.Title>Confirmar Eliminación</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            ¿Estás seguro de que deseas eliminar este producto?
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleCloseEliminarConfirmar}>
-              Cancelar
-            </Button>
-            <Button variant="danger" onClick={handleEliminar}>
-              Eliminar
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </div>
     );
-  }
-  
-  export default GestionPW;
+};
+
+export default GestionPW;
