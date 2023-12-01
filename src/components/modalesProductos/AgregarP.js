b2c08a6fa86cf2949cf677ea1989b29a5005663c
BoroaDev7 <jmborjas@unitec.edu>
2023-12-01 09:11:42 -0600

    Agregar Productos Terminado

--- src/components/modalesProductos/AgregarP.js
+++ src/components/modalesProductos/AgregarP.js
@@ -1,97 +1,213 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./ModificarP.css";
+import CustomAlert from "../Informative_screens/CustomAlert.js";
 
-import './ModificarP.css';
+function AgregarP() {
+  const navigate = useNavigate();
 
-const AgregarP = ({ product, onSave }) => {
-    // Estado para las características
-    const [caracteristicas, setCaracteristicas] = useState(product?.caracteristicas || '');
-    const navigate = useNavigate();
+  const mostrarAlerta = (message, variant) => {
+    setAlertVariant(variant);
+    setAlertMessage(message);
+    setShowAlert(true);
 
-    // Estado para las imágenes
-    const [imagenes, setImagenes] = useState([]);
-    const handleCancel = () => {
-        // Redireccionar al usuario al componente deseado al cancelar
-        navigate('/gestionpw'); // Reemplaza '/ruta-deseada' con tu ruta específica
+    setTimeout(() => {
+      setShowAlert(false);
+    }, 2400);
+  };
+
+  const [nombre, setNombre] = useState("");
+
+  const [modelo, setModelo] = useState("");
+
+  const [categoria, setCategoria] = useState("");
+
+  const [descripcion, setDescripcion] = useState("");
+
+  const [caracteristicas, setCaracteristicas] = useState("");
+
+  const [showAlert, setShowAlert] = useState(false);
+
+  const [alertMessage, setAlertMessage] = useState("");
+
+  const [alertVariant, setAlertVariant] = useState("white");
+
+  const [cantidad, setCantidad] = useState("");
+
+  const [precio, setPrecio] = useState("");
+
+  const [imagenes, setImagenes] = useState([]);
+
+  const [userType, setUserType] = useState("");
+  const handleCancel = () => {
+    navigate("/gestionpw");
+  };
+
+  const handleSave = async () => {
+    setUserType(localStorage.getItem("userType"));
+    const cambios = {
+      Nombre: nombre,
+      Modelo: modelo,
+      Categoria: categoria,
+      Descripcion: descripcion,
+      Caracteristicas: caracteristicas,
+      Cantidad: cantidad,
+      Precio: precio,
+      Imagenes: imagenes,
+      userCreatingType: "*",
     };
-    const handleSave = () => {
-        // Lógica para guardar los cambios
-        const cambios = {
-            nombre: '', // Obtén el valor del campo de nombre
-            modelo: '', // Obtén el valor del campo de modelo
-            descripcion: '', // Obtén el valor del campo de descripción
-            caracteristicas: caracteristicas, // Usa el estado para las características
-            precio: '', // Obtén el valor del campo de precio
-            imagenes: imagenes, // Usa el estado para las imágenes
-        };
 
-        // Llama a la función 'onSave' y pasa los cambios
-        onSave(cambios);
-    };
+    try {
+      if (
+        nombre === "" ||
+        modelo === "" ||
+        precio === "" ||
+        categoria === "" ||
+        cantidad === ""
+      ) {
+        mostrarAlerta("Todos los campos son obligatorios", "danger");
+        return;
+      }
+      const response = await fetch("http://localhost:3000/agregarProducto", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(cambios),
+      });
 
-    const handleImagenesChange = (e) => {
-        // Manejo de cambios en la carga de imágenes
-        const files = e.target.files;
-        const nuevasImagenes = [];
+      const errorData = await response.json();
+      if (!response.ok) {
+        throw new Error(`Error: ${errorData.message || response.status}`);
+      } else {
+        mostrarAlerta("Producto agregado correctamente", "success");
+      }
+      return;
+    } catch (error) {
+      mostrarAlerta("Error al agregar producto ", "danger");
+    }
 
-        for (let i = 0; i < files.length; i++) {
-            const file = files[i];
-            nuevasImagenes.push(URL.createObjectURL(file));
-        }
+    console.log(cambios);
+  };
 
-        setImagenes(nuevasImagenes);
-    };
+  const handleImagenesChange = (e) => {
+    const files = e.target.files;
+    const nuevasImagenes = [];
 
-    return (
-        <div className="modificar-producto">
-            <button className="btn-regresar" onClick={handleCancel}>
-                Regresar
-            </button>
-            <h2>Agregar Producto</h2>
-            <div className="form-group">
-                <label>Nombre de Producto</label>
-                <input type="text" placeholder="" />
-            </div>
-            <div className="form-group">
-                <label>Modelo</label>
-                <input type="text" placeholder="" />
-            </div>
-            <div className="form-group">
-                <label>Descripción</label>
-                <textarea placeholder=""></textarea>
-            </div>
-            <div className="form-group">
-                <label>Características</label>
-                <textarea
-                    //value={caracteristicas}
-                    // onChange={(e) => setCaracteristicas(e.target.value)}
-                    placeholder=""
-                ></textarea>
-            </div>
-            <div className="form-group">
-                <label>Imágenes</label>
-                <input type="file" accept="image/*" multiple onChange={handleImagenesChange} />
-                <div className="imagenes-preview">
-                    {imagenes.map((imagen, index) => (
-                        <img key={index} src={imagen} alt={`Imagen ${index}`} />
-                    ))}
-                </div>
-            </div>
-            <div className="form-group">
-                <label>Precio</label>
-                <input type="text" placeholder="" />
-            </div>
-            <div className="buttons">
-                <button className="btn-cancelar" onClick={handleCancel}>
-                    Cancelar
-                </button>
-                <button className="btn-modificar" onClick={handleSave}>
-                    Agregar Producto
-                </button>
-            </div>
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      nuevasImagenes.push(URL.createObjectURL(file));
+    }
+    setImagenes(nuevasImagenes);
+  };
 
+  return (
+    <div className="modificar-producto">
+      <button className="btn-regresar" onClick={handleCancel}>
+        Regresar
+      </button>
+      <h2>Agregar Producto</h2>
+      <div className="form-group">
+        <label>Nombre de Producto</label>
+        <input
+          type="text"
+          placeholder=""
+          id="nombreProducto"
+          value={nombre}
+          onChange={(e) => setNombre(e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <label>Modelo</label>
+        <input
+          type="text"
+          placeholder=""
+          id="modeloProducto"
+          value={modelo}
+          onChange={(e) => setModelo(e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <label>Categoria</label>
+        <input
+          type="text"
+          placeholder=""
+          id="categoriaProducto"
+          value={categoria}
+          onChange={(e) => setCategoria(e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <label>Descripción</label>
+        <textarea
+          placeholder=""
+          id="descripcionProducto"
+          value={descripcion}
+          onChange={(e) => setDescripcion(e.target.value)}
+        ></textarea>
+      </div>
+      <div className="form-group">
+        <label>Características</label>
+        <textarea
+          placeholder=""
+          id="caracteristicasProducto"
+          value={caracteristicas}
+          onChange={(e) => setCaracteristicas(e.target.value)}
+        ></textarea>
+      </div>
+      <div className="form-group">
+        <label>Imágenes</label>
+        <input
+          type="file"
+          accept="image/*"
+          multiple
+          onChange={handleImagenesChange}
+          id="imagenesProducto"
+        />
+        <div className="imagenes-preview">
+          {imagenes.map((imagen, index) => (
+            <img key={index} src={imagen} alt={`Imagen ${index}`} />
+          ))}
         </div>
-    );
-};
+      </div>
+      <div className="form-group">
+        <label>Cantidad</label>
+        <input
+          type="text"
+          placeholder=""
+          id="cantidadProducto"
+          value={cantidad}
+          onChange={(e) => setCantidad(e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <label>Precio</label>
+        <input
+          type="text"
+          placeholder=""
+          id="precioProducto"
+          value={precio}
+          onChange={(e) => setPrecio(e.target.value)}
+        />
+      </div>
+      {showAlert && (
+        <CustomAlert
+          className="alerta"
+          message={alertMessage}
+          variant={alertVariant}
+          onClose={() => setShowAlert(false)}
+        />
+      )}
+      <div className="buttons">
+        <button className="btn-cancelar" onClick={handleCancel}>
+          Cancelar
+        </button>
+        <button className="btn-modificar" onClick={handleSave}>
+          Agregar Producto
+        </button>
+      </div>
+    </div>
+  );
+}
 
 export default AgregarP;
