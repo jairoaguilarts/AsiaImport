d786100f1883bf02ac304966f5f8ef7f39bed6af
jairoaguilarts <jairoaguilar1219@gmail.com>
2023-12-13 17:49:01 -0600

    implementando carrito con el back

--- src/components/Client_screens/Carrito.js
+++ src/components/Client_screens/Carrito.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Carrito.css';
 import minus from '../../assets/minus.png';
 import plus from '../../assets/plus.png';
@@ -10,15 +10,13 @@ import closeIcon from '../../assets/close.png';
 import { Link } from 'react-router-dom';
 
 const Carrito = ({ onClose }) => {
-    const [productos, setProductos] = useState([
-        { id: 1, nombre: 'Parlante Bluetooth con Luz Led', cantidad: 1, precio: 1500.00 },
-        { id: 2, nombre: 'Audifonos Bluetooth', cantidad: 2, precio: 900.00 },
-    ]);
+    const [productos, setProductos] = useState([]);
 
-    const subtotal = productos.reduce((total, producto) => total + producto.cantidad * producto.precio, 0);
+    const [subtotal, setSubtotal] = useState(0);
     const [entrega, setEntrega] = useState(0);
-    const [totalCarrito, setTotalCarrito] = useState(subtotal + entrega);
+    const [totalCarrito, setTotalCarrito] = useState(0);
     const [carritoVisible, setCarritoVisible] = useState(true);
+
     const handleChange = (id, e) => {
         setProductos(
             productos.map(producto =>
@@ -27,10 +25,48 @@ const Carrito = ({ onClose }) => {
         );
     };
 
-    const handleChangeCantidad = (id, cambio) => {
+    useEffect(() => {
+        const fetchCarrito = async () => {
+            const firebaseUID = localStorage.getItem("FireBaseUID");
+
+            try {
+                const response = await fetch(`http://localhost:3000/obtenerCarrito/${firebaseUID}`, {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                });
+
+                if (!response.ok) {
+                    const errorData = await response.json();
+                    throw new Error(`Error: ${errorData.message || response.status}`);
+                }
+                const data = await response.json();
+                const productosConCantidad = data.map(producto => ({ ...producto, cantidad: 1 }));
+                setProductos(productosConCantidad);
+            } catch (error) {
+                console.log('Error al obtener los productos del carrito: ', error);
+                alert("Error");
+            }
+        };
+
+        fetchCarrito();
+    }, []);
+    
+    useEffect(() => {
+        const nuevoSubtotal = productos.reduce((total, producto) => {
+            const cantidad = Number(producto.cantidad) || 0;
+            const precio = Number(producto.Precio) || 0;
+            return total + cantidad * precio;
+        }, 0);
+        setSubtotal(nuevoSubtotal);
+        setTotalCarrito(nuevoSubtotal + entrega);
+    }, [productos, entrega]);
+
+    const handleChangeCantidad = (modelo, cambio) => {
         setProductos(
             productos.map(producto =>
-                producto.id === id ? { ...producto, cantidad: Math.max(0, producto.cantidad + cambio) } : producto
+                producto.Modelo === modelo ? { ...producto, cantidad: Math.max(1, producto.cantidad + cambio) } : producto
             )
         );
     };
@@ -41,34 +77,23 @@ const Carrito = ({ onClose }) => {
     };
 
     const handleCloseCarrito = () => {
-        onClose(); // Este método se pasa desde el padre (Navibar) para cerrar el carrito
-    };
-    const handleRemoveProducto = (id) => {
-        // Actualizar el estado de productos eliminando el producto con el id dado
-        setProductos(productos.filter(producto => producto.id !== id));
-
-        // Si también necesitas actualizar el total del carrito después de eliminar un producto,
-        // asegúrate de hacerlo aquí también. Por ejemplo:
-        const newSubtotal = productos.reduce((total, producto) => {
-            if (producto.id !== id) {
-                return total + producto.cantidad * producto.precio;
-            }
-            return total;
-        }, 0);
-
-        setTotalCarrito(newSubtotal + entrega);
-    };
-    const handlePagarPedido = () => {
-        // Cierra el carrito llamando a la función onClose
         onClose();
-
     };
+    const handleRemoveProducto = (modelo) => {
+        const nuevosProductos = productos.filter(producto => producto.Modelo !== modelo);
+        setProductos(nuevosProductos);
+    };
+
+    const handlePagarPedido = () => {
+        onClose();
+    };
+
     return (
         <>
             {carritoVisible && (
                 <div className="carrito">
                     <button className="close-btn" onClick={onClose}>
-                        <img src={closeIcon} alt="Cerrar" /> {/* Reemplaza la X con una imagen */}
+                        <img src={closeIcon} alt="Cerrar" />
                     </button>
                     <div className="carrito-header">
                         <img src={cartIcon2} className="card-icon" alt="Icono del carrito" />
@@ -78,25 +103,25 @@ const Carrito = ({ onClose }) => {
                     {productos.map(producto => (
                         <div className="producto" key={producto.id}>
                             <div className="producto-info">
-                                <img src={producto.id === 1 ? imagenParlante : imagenAudifonos} alt={producto.nombre} />
-                                <span className="producto-nombre">{producto.nombre}</span>
+                                <img src={producto.ImagenID} alt={producto.Nombre} />
+                                <span className="producto-nombre">{producto.Nombre}</span>
                             </div>
                             <div className="producto-cantidad">
-                                <button onClick={() => handleChangeCantidad(producto.id, -1)}>
+                                <button onClick={() => handleChangeCantidad(producto.Modelo, -1)}>
                                     <img src={minus} alt="Menos" />
                                 </button>
                                 <input
                                     type="number"
-                                    readOnly // Esto hace que el input sea de solo lectura
+                                    readOnly
                                     value={producto.cantidad}
-                                    onChange={(e) => handleChange(producto.id, e)} // Esta línea ya no será necesaria si es de solo lectura
+                                    onChange={(e) => handleChange(producto.Modelo, e)}
                                 />
-                                <button onClick={() => handleChangeCantidad(producto.id, 1)}>
+                                <button onClick={() => handleChangeCantidad(producto.Modelo, 1)}>
                                     <img src={plus} alt="Más" />
                                 </button>
                             </div>
-                            <div className="producto-precio">L {producto.precio.toFixed(2)}</div>
-                            <button onClick={() => handleRemoveProducto(producto.id)}>
+                            <div className="producto-precio">L {producto.Precio}</div>
+                            <button onClick={() => handleRemoveProducto(producto.Modelo)}>
                                 <img src={deleteIcon} alt="Eliminar" />
                             </button>
                         </div>
@@ -104,25 +129,21 @@ const Carrito = ({ onClose }) => {
                     <div className="totales">
                         <div className="subtotal">
                             <span>Subtotal</span>
-                            <span>L {subtotal.toFixed(2)}</span>
+                            <span>L{subtotal.toFixed(2)}</span>
                         </div>
                         <div className="divisor"></div>
                         <div className="total">
                             <span>Total</span>
-                            <span>L {totalCarrito.toFixed(2)}</span>
+                            <span>L{totalCarrito.toFixed(2)}</span>
                         </div>
                     </div>
                     <div className="pagar-btn">
-
                         <Link to="/compra">
                             <button className="pagar-btn" onClick={handlePagarPedido}>
                                 PAGAR PEDIDO
                             </button>
-
                         </Link>
-
                     </div>
-
                 </div>
             )}
         </>
