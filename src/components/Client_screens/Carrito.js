b6164bac5f1184db4f720f9d6b6481015775eaa3
jairoaguilarts <jairoaguilar1219@gmail.com>
2023-12-18 11:13:45 -0600

    agregando mensaje para cuando no ha iniciado sesion

--- src/components/Client_screens/Carrito.js
+++ src/components/Client_screens/Carrito.js
@@ -3,11 +3,11 @@ import './Carrito.css';
 import minus from '../../assets/minus.png';
 import plus from '../../assets/plus.png';
 import cartIcon2 from '../../assets/add-to-cart.png';
-import imagenAudifonos from '../../assets/Audifonos UltraSound.png';
-import imagenParlante from '../../assets/Parlante Bluetooth.png';
+import CustomAlert from "../Informative_screens/CustomAlert.js";
 import deleteIcon from '../../assets/delete.png';
 import closeIcon from '../../assets/close.png';
 import { Link } from 'react-router-dom';
+import { fireEvent } from '@testing-library/react';
 
 const Carrito = ({ onClose }) => {
     const [productos, setProductos] = useState([]);
@@ -16,6 +16,19 @@ const Carrito = ({ onClose }) => {
     const [entrega, setEntrega] = useState(0);
     const [totalCarrito, setTotalCarrito] = useState(0);
     const [carritoVisible, setCarritoVisible] = useState(true);
+    const [showAlert, setShowAlert] = useState(false);
+    const [alertMessage, setAlertMessage] = useState("");
+    const [alertVariant, setAlertVariant] = useState("white");
+
+    const mostrarAlerta = (message, variant) => {
+        setAlertVariant(variant);
+        setAlertMessage(message);
+        setShowAlert(true);
+
+        setTimeout(() => {
+            setShowAlert(false);
+        }, 24000);
+    };
 
     const handleChange = (id, e) => {
         setProductos(
@@ -29,24 +42,28 @@ const Carrito = ({ onClose }) => {
         const fetchCarrito = async () => {
             const firebaseUID = localStorage.getItem("FireBaseUID");
 
-            try {
-                const response = await fetch(`https://importasia-api.onrender.com/obtenerCarrito/${firebaseUID}`, {
-                    method: "GET",
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-                });
+            if (firebaseUID !== null) {
+                try {
+                    const response = await fetch(`https://importasia-api.onrender.com/obtenerCarrito/${firebaseUID}`, {
+                        method: "GET",
+                        headers: {
+                            "Content-Type": "application/json"
+                        },
+                    });
 
-                if (!response.ok) {
-                    const errorData = await response.json();
-                    throw new Error(`Error: ${errorData.message || response.status}`);
+                    if (!response.ok) {
+                        const errorData = await response.json();
+                        throw new Error(`Error: ${errorData.message || response.status}`);
+                    }
+                    const data = await response.json();
+                    const productosConCantidad = data.map(producto => ({ ...producto, cantidad: 1 }));
+                    setProductos(productosConCantidad);
+                } catch (error) {
+                    console.log('Error al obtener los productos del carrito: ', error);
+                    alert("Error");
                 }
-                const data = await response.json();
-                const productosConCantidad = data.map(producto => ({ ...producto, cantidad: 1 }));
-                setProductos(productosConCantidad);
-            } catch (error) {
-                console.log('Error al obtener los productos del carrito: ', error);
-                alert("Error");
+            } else {
+                mostrarAlerta("No se ha iniciado sesion.", "danger");
             }
         };
 
@@ -120,50 +137,63 @@ const Carrito = ({ onClose }) => {
                         <h2>CARRITO</h2>
                     </div>
                     <div className="divisor"></div>
-                    {productos.map(producto => (
-                        <div className="producto" key={producto.id}>
-                            <div className="producto-info">
-                                <img src={producto.ImagenID} alt={producto.Nombre} />
-                                <span className="producto-nombre">{producto.Nombre}</span>
+                    {showAlert ? (
+                        <>
+                            <CustomAlert
+                                className="alerta"
+                                message={alertMessage}
+                                variant={alertVariant}
+                                onClose={() => setShowAlert(false)}
+                            />
+                        </>
+                    ) : (
+                        <>
+                            {productos.map(producto => (
+                                <div className="producto" key={producto.id}>
+                                    <div className="producto-info">
+                                        <img src={producto.ImagenID} alt={producto.Nombre} />
+                                        <span className="producto-nombre">{producto.Nombre}</span>
+                                    </div>
+                                    <div className="producto-cantidad">
+                                        <button onClick={() => handleChangeCantidad(producto.Modelo, -1)}>
+                                            <img src={minus} alt="Menos" />
+                                        </button>
+                                        <input
+                                            type="number"
+                                            readOnly
+                                            value={producto.cantidad}
+                                            onChange={(e) => handleChange(producto.Modelo, e)}
+                                        />
+                                        <button onClick={() => handleChangeCantidad(producto.Modelo, 1)}>
+                                            <img src={plus} alt="Más" />
+                                        </button>
+                                    </div>
+                                    <div className="producto-precio">L {producto.Precio}</div>
+                                    <button onClick={() => handleRemoveProducto(producto.Modelo)}>
+                                        <img src={deleteIcon} alt="Eliminar" />
+                                    </button>
+                                </div>
+                            ))}
+                            <div className="totales">
+                                <div className="subtotal">
+                                    <span>Subtotal</span>
+                                    <span>L{subtotal.toFixed(2)}</span>
+                                </div>
+                                <div className="divisor"></div>
+                                <div className="total">
+                                    <span>Total</span>
+                                    <span>L{totalCarrito.toFixed(2)}</span>
+                                </div>
                             </div>
-                            <div className="producto-cantidad">
-                                <button onClick={() => handleChangeCantidad(producto.Modelo, -1)}>
-                                    <img src={minus} alt="Menos" />
-                                </button>
-                                <input
-                                    type="number"
-                                    readOnly
-                                    value={producto.cantidad}
-                                    onChange={(e) => handleChange(producto.Modelo, e)}
-                                />
-                                <button onClick={() => handleChangeCantidad(producto.Modelo, 1)}>
-                                    <img src={plus} alt="Más" />
-                                </button>
+                            <div className="pagar-btn">
+                                <Link to="/compra">
+                                    <button className="pagar-btn" onClick={handlePagarPedido}>
+                                        PAGAR PEDIDO
+                                    </button>
+                                </Link>
                             </div>
-                            <div className="producto-precio">L {producto.Precio}</div>
-                            <button onClick={() => handleRemoveProducto(producto.Modelo)}>
-                                <img src={deleteIcon} alt="Eliminar" />
-                            </button>
-                        </div>
-                    ))}
-                    <div className="totales">
-                        <div className="subtotal">
-                            <span>Subtotal</span>
-                            <span>L{subtotal.toFixed(2)}</span>
-                        </div>
-                        <div className="divisor"></div>
-                        <div className="total">
-                            <span>Total</span>
-                            <span>L{totalCarrito.toFixed(2)}</span>
-                        </div>
-                    </div>
-                    <div className="pagar-btn">
-                        <Link to="/compra">
-                            <button className="pagar-btn" onClick={handlePagarPedido}>
-                                PAGAR PEDIDO
-                            </button>
-                        </Link>
-                    </div>
+                        </>
+                    )}
                 </div>
             )}
         </>
