d2335c8a2865d1801920b4b580d1283c2852e705
BoroaDev7 <jmborjas@unitec.edu>
2024-02-07 09:14:28 -0600

    Cambios Paginacion en las ordenes

--- src/components/Admin_screens/GestionOrdenes.js
+++ src/components/Admin_screens/GestionOrdenes.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./GestionOrdenes.css";
+import Pagination from "../Client_screens/Pagination";
 
 const GestionOrdenes = () => {
   const [busqueda, setBusqueda] = useState("");
@@ -11,27 +12,29 @@ const GestionOrdenes = () => {
   const [isDetallePopupVisible, setIsDetallePopupVisible] = useState(false);
   const [ordenDetalle, setOrdenDetalle] = useState(null);
   const [filtroEstado, setFiltroEstado] = useState("Cualquiera");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [ordersPerPage] = useState(6);
 
   const handleBusquedaChange = (e) => {
     const terminoBusqueda = e.target.value;
     setBusqueda(terminoBusqueda);
     filtrarOrdenes(terminoBusqueda, filtroEstado);
   };
+  
   const handleEstadoChange = (e) => {
     const estadoSeleccionado = e.target.value;
     setFiltroEstado(estadoSeleccionado);
     filtrarOrdenes(busqueda, estadoSeleccionado);
   };
+  
   const filtrarOrdenes = (terminoBusqueda, estado) => {
     let ordenesTemp = ordenes;
     if (terminoBusqueda) {
       ordenesTemp = ordenesTemp.filter(
         (orden) =>
           orden._id.toLowerCase().includes(terminoBusqueda.toLowerCase()) ||
-          orden.estadoOrden
-            .toLowerCase()
-            .includes(terminoBusqueda.toLowerCase()) ||
-          orden.tipoOrden.toLowerCase().includes(terminoBusqueda.toLowerCase()) // Línea agregada para filtrar por tipo de orden
+          orden.estadoOrden.toLowerCase().includes(terminoBusqueda.toLowerCase()) ||
+          orden.tipoOrden.toLowerCase().includes(terminoBusqueda.toLowerCase())
       );
     }
     if (estado !== "Cualquiera") {
@@ -44,21 +47,24 @@ const GestionOrdenes = () => {
     setOrdenesFiltradas(ordenes);
   }, [ordenes]);
 
+  useEffect(() => {
+    fetch(`https://importasia-api.onrender.com/ordenes`)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        console.log(response);
+        throw response;
+      })
+      .then((data) => setOrdenes(data))
+      .catch((error) => {
+        console.error("Error al recuperar las órdenes:", error);
+      });
+  }, []);
+
   const mostrarPopupDetalle = (orden) => {
     setOrdenDetalle(orden);
     setIsDetallePopupVisible(true);
-  };
-  const getEstadoClass = (estado) => {
-    switch (estado) {
-      case "En Proceso":
-        return "estado-en-proceso";
-      case "Verificada":
-        return "estado-verificada";
-      case "Completada":
-        return "estado-completado";
-      default:
-        return "";
-    }
   };
 
   const PopupDetalleOrden = () => {
@@ -77,32 +83,17 @@ const GestionOrdenes = () => {
                 <li>Departamento: {ordenDetalle.detalles.departamento}</li>
                 <li>Municipio: {ordenDetalle.detalles.municipio}</li>
                 <li>Dirección: {ordenDetalle.detalles.direccion}</li>
-                <li>
-                  Punto de Referencia: {ordenDetalle.detalles.puntoreferencia}
-                </li>
+                <li>Punto de Referencia: {ordenDetalle.detalles.puntoreferencia}</li>
               </>
             )}
             <li>Teléfono: {ordenDetalle.detalles.numerotelefono}</li>
             <li>Nombre de Usuario: {ordenDetalle.nombre_usuario}</li>
             {ordenDetalle.tipoOrden === "pickup" && (
-              <li>
-                Identidad de Usuario: {ordenDetalle.detalles.identidadUsuario}
-              </li>
+              <li>Identidad de Usuario: {ordenDetalle.detalles.identidadUsuario}</li>
             )}
-            <li>
-              Fecha:{" "}
-              {new Date(
-                ordenDetalle.detalles.fecha_ingreso
-              ).toLocaleDateString()}
-            </li>
-            <li>
-              {" "}
-              Estado:{" "}
-              <span className={getEstadoClass(ordenDetalle.estadoOrden)}>
-                {ordenDetalle.estadoOrden}
-              </span>
-            </li>
-            <li> Estado Pago: {ordenDetalle.estadoPago}</li>
+            <li>Fecha: {new Date(ordenDetalle.detalles.fecha_ingreso).toLocaleDateString()}</li>
+            <li>Estado: <span className={getEstadoClass(ordenDetalle.estadoOrden)}>{ordenDetalle.estadoOrden}</span></li>
+            <li>Estado Pago: {ordenDetalle.estadoPago}</li>
           </ul>
           <div>
             <strong>Articulos en la Orden:</strong>
@@ -126,98 +117,25 @@ const GestionOrdenes = () => {
           <div>
             <strong>Total de la Orden:{ordenDetalle.total}</strong>
           </div>
-          <button
-            className="button-detalle3"
-            onClick={() => setIsDetallePopupVisible(false)}
-          >
-            Cerrar
-          </button>
+          <button className="button-detalle3" onClick={() => setIsDetallePopupVisible(false)}>Cerrar</button>
         </div>
       </div>
     );
   };
 
-  useEffect(() => {
-    fetch(`https://importasia-api.onrender.com/ordenes`)
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        }
-        console.log(response);
-        throw response;
-      })
-      .then((data) => setOrdenes(data))
-      .catch((error) => {
-        console.error("Error al recuperar las órdenes:", error);
-      });
-  }, []);
-  const mostrarPopup = (orden) => {
-    setOrdenActual(orden);
-    setIsPopupVisible(true);
+  const getEstadoClass = (estado) => {
+    switch (estado) {
+      case "En Proceso":
+        return "estado-en-proceso";
+      case "Verificada":
+        return "estado-verificada";
+      case "Completada":
+        return "estado-completado";
+      default:
+        return "";
+    }
   };
-  const PopupEstadoOrden = () => {
-    return isPopupVisible ? (
-      <div className="popup">
-        <div className="popup-inner">
-          <h3>Actualizar Estado de la Orden</h3>
-          <hr></hr>
-          <form>
-            <label
-              style={{
-                color:
-                  estadoSeleccionado === "En Proceso" ? "#D8750D" : "inherit",
-              }}
-            >
-              <input
-                type="radio"
-                name="estado"
-                value="En Proceso"
-                onChange={(e) => setEstadoSeleccionado(e.target.value)}
-                checked={estadoSeleccionado === "En Proceso"}
-              />
-              En proceso
-            </label>
-            <label
-              style={{
-                color:
-                  estadoSeleccionado === "Verificada" ? "#D8A20D" : "inherit",
-              }}
-            >
-              <input
-                type="radio"
-                name="estado"
-                value="Verificada"
-                onChange={(e) => setEstadoSeleccionado(e.target.value)}
-                checked={estadoSeleccionado === "Verificada"}
-              />
-              Verificada
-            </label>
-            <label
-              style={{
-                color:
-                  estadoSeleccionado === "Completada" ? "green" : "inherit",
-              }}
-            >
-              <input
-                type="radio"
-                name="estado"
-                value="Completada"
-                onChange={(e) => setEstadoSeleccionado(e.target.value)}
-                checked={estadoSeleccionado === "Completada"}
-              />
-              Completado
-            </label>
-            <button type="button" onClick={actualizarEstado}>
-              Actualizar Estado
-            </button>
-            <button type="button" onClick={() => setIsPopupVisible(false)}>
-              Cerrar
-            </button>
-          </form>
-        </div>
-      </div>
-    ) : null;
-  };
+
   const actualizarEstado = () => {
     const url = `https://importasia-api.onrender.com/actualizarEstado`;
     const data = {
@@ -276,6 +194,14 @@ const GestionOrdenes = () => {
     return <div>Detalles no disponibles</div>;
   };
 
+  // Función para manejar el cambio de página
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  // Lógica para obtener las órdenes actuales según la página
+  const indexOfLastOrder = currentPage * ordersPerPage;
+  const indexOfFirstOrder = indexOfLastOrder - ordersPerPage;
+  const currentOrders = ordenesFiltradas.slice(indexOfFirstOrder, indexOfLastOrder);
+
   return (
     <div>
       <h1 className="titulo-ordenes">Órdenes</h1>
@@ -319,8 +245,8 @@ const GestionOrdenes = () => {
             </tr>
           </thead>
           <tbody>
-            {ordenesFiltradas.map((orden) => (
-              <tr key={orden.order_id}>
+            {currentOrders.map((orden) => (
+              <tr key={orden._id}>
                 <td>{orden._id}</td>
                 <td>{orden.tipoOrden}</td>
                 <td>{orden.detalles ? obtenerDetalles(orden) : "N/A"}</td>
@@ -331,7 +257,7 @@ const GestionOrdenes = () => {
                 <td>
                   <button
                     className="button-gestion mod-estado-orden"
-                    onClick={() => mostrarPopup(orden)}
+                    onClick={() => setIsPopupVisible(true)}
                   >
                     Estado de Orden
                   </button>
@@ -347,7 +273,12 @@ const GestionOrdenes = () => {
           </tbody>
         </table>
       </div>
-      <PopupEstadoOrden />
+      <Pagination
+        productsPerPage={ordersPerPage}
+        totalProducts={ordenesFiltradas.length}
+        paginate={paginate}
+        currentPage={currentPage}
+      />
       <PopupDetalleOrden />
     </div>
   );
