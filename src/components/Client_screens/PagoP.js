0b710f66cac97478f1f52438b529938ede4076b0
BoroaDev7 <jmborjas@unitec.edu>
2024-02-08 21:55:34 -0600

    Cambios correo orden efectivo

--- src/components/Client_screens/PagoP.js
+++ src/components/Client_screens/PagoP.js
@@ -42,40 +42,41 @@ function PagoP() {
         body: JSON.stringify(dataOrden),
       }
     );
-    const formData = {
-      _orderId: responseOrdenData._id,
-      tipoOrden: "Delivery",
-      Fecha: new Date().toISOString(),
-      carrito: responseOrdenData.carrito,
-      cantidades: responseOrdenData.cantidades,
-      total: responseOrdenData.total,
-      correo: responseOrdenData.correo,
-    };
-
-    const requestOptions = {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(formData),
-    };
-
-    try {
-      const mandarOrden = await fetch(
-        "https://importasia-api.onrender.com/send-orderDetails",
-        requestOptions
-      );
-
-      if (!mandarOrden.ok) {
-        const errorMessage = await mandarOrden.text();
-        throw new Error(errorMessage);
-      }
-
-      console.log("Orden enviada al correo con éxito.");
-    } catch (error) {
-      console.error("Error al enviar la orden:", error);
-    }
 
     if (responseOrden.ok) {
       const responseOrdenData = await responseOrden.json();
+      const formData = {
+        _orderId: responseOrdenData._id,
+        tipoOrden: "Delivery",
+        Fecha: new Date().toISOString(),
+        carrito: responseOrdenData.carrito,
+        cantidades: responseOrdenData.cantidades,
+        total: responseOrdenData.total,
+        correo: responseOrdenData.correo,
+      };
+
+      const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      };
+
+      try {
+        const mandarOrden = await fetch(
+          "https://importasia-api.onrender.com/send-orderDetails",
+          requestOptions
+        );
+
+        if (!mandarOrden.ok) {
+          const errorMessage = await mandarOrden.text();
+          throw new Error(errorMessage);
+        }
+
+        console.log("Orden enviada al correo con éxito.");
+      } catch (error) {
+        console.error("Error al enviar la orden:", error);
+      }
+
       setOrdenId(responseOrdenData._id);
       const userActualizacion = {
         carritoCompras: [],
