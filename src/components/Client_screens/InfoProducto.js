9e36016a228f0e3c7663e06bd1cd2129e17a6212
Jairo Aguilar <jairoaguilar1219@gmail.com>
2024-02-12 12:39:11 -0600

    implementado reseñas

--- src/components/Client_screens/InfoProducto.js
+++ src/components/Client_screens/InfoProducto.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import CustomAlert from "../Informative_screens/CustomAlert";
 import "./InfoProducto.css";
+import Form from "react-bootstrap/Form";
+import Swal from "sweetalert2";
 import audifonosProduct1 from "../../assets/Srhythm.png";
 
 function InfoAudifonos() {
@@ -22,6 +24,10 @@ function InfoAudifonos() {
   const [showAlert, setShowAlert] = useState(false);
   const [alertMessage, setAlertMessage] = useState("");
   const [alertVariant, setAlertVariant] = useState("white");
+  const [isAgregarR, setIsAgregarR] = useState(false);
+  const [titulo, setTitulo] = useState("");
+  const [comentario, setComentario] = useState("");
+  const [calificacion, setCalificacion] = useState(0);
 
   useEffect(() => {
     const fetchProduct = async () => {
@@ -41,6 +47,41 @@ function InfoAudifonos() {
 
     fetchProduct();
   }, []);
+
+  const handleAgregarResena = async () => {
+    const userFirebaseUID = localStorage.getItem("FireBaseUID");
+    const Modelo = modelo;
+
+    const data = {
+      userFirebaseUID,
+      Modelo,
+      Calificacion: calificacion,
+      Titulo: titulo,
+      Comentario: comentario,
+    };
+
+    const response = await fetch('https://importasia-api.onrender.com/agregarResena',
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }
+    );
+
+    if(response.ok) {
+      Swal.fire({
+        icon: "success",
+        title: "¡Muchas gracias por tu comentario!",
+        text: "Nos ayuda a brindar los mejores productos a nuestros usuarios.",
+      });
+      setIsAgregarR(false);
+      setCalificacion(0);
+      setTitulo("");
+      setComentario("");
+    }
+  };
 
   const handleAgregar = async () => {
     const firebaseUID = localStorage.getItem("FireBaseUID");
@@ -62,10 +103,10 @@ function InfoAudifonos() {
         const errorData = await response.json();
         throw new Error(`Error: ${errorData.message || response.status}`);
       }
-      mostrarAlerta("Agregado al carrito con éxito", "success"); // Usa 'success' o la variante que tengas definida para éxitos
+      mostrarAlerta("Agregado al carrito con éxito", "success");
     } catch (error) {
       console.log("Error: ", error);
-      mostrarAlerta("Error al agregar al carrito", "danger"); // Usa 'danger' o la variante que tengas definida para errores
+      mostrarAlerta("Error al agregar al carrito", "danger");
     }
   };
 
@@ -126,8 +167,7 @@ function InfoAudifonos() {
               <h2>Descripción</h2>
               <ul className="product-description">
                 {producto.Descripcion}
-              </ul>{" "}
-              {/* Agregada clase product-description */}
+              </ul>
             </div>
           )}
           {activeTab === "features" && (
@@ -135,12 +175,56 @@ function InfoAudifonos() {
               <h2>Características</h2>
               <ul className="product-features">
                 {producto.Caracteristicas}
-              </ul>{" "}
-              {/* Agregada clase product-features */}
+              </ul>
             </div>
           )}
         </div>
       )}
+      <div className="tab-content">
+        <button className="agregar-resena" onClick={() => setIsAgregarR(true)}>
+          Agregar Reseña
+        </button>
+        {isAgregarR && (
+          <>
+            <p>Titulo</p>
+            <Form.Control
+              id="Titulo"
+              className="contenedores"
+              type="text"
+              placeholder="Ingrese un titulo para la reseña"
+              value={titulo}
+              onChange={(e) => setTitulo(e.target.value)}
+            />
+            <>
+              <div class="rating">
+                <input value="5" name="rate" id="star5" type="radio" checked={calificacion === 5} onChange={() => setCalificacion(5)} />
+                <label title="text" for="star5"></label>
+                <input value="4" name="rate" id="star4" type="radio" checked={calificacion === 4} onChange={() => setCalificacion(4)} />
+                <label title="text" for="star4"></label>
+                <input value="3" name="rate" id="star3" type="radio" checked={calificacion === 3} onChange={() => setCalificacion(3)} />
+                <label title="text" for="star3"></label>
+                <input value="2" name="rate" id="star2" type="radio" checked={calificacion === 2} onChange={() => setCalificacion(2)} />
+                <label title="text" for="star2"></label>
+                <input value="1" name="rate" id="star1" type="radio" checked={calificacion === 1} onChange={() => setCalificacion(1)} />
+                <label title="text" for="star1"></label>
+              </div>
+            </>
+            <p>Comentario</p>
+            <Form.Control
+              id="comentario"
+              className="contenedores"
+              type="text"
+              as="textarea"
+              placeholder="Ingrese un comentario"
+              value={comentario}
+              onChange={(e) => setComentario(e.target.value)}
+            />
+            <button className="agregar-resena" onClick={handleAgregarResena}>
+              Agregar
+            </button>
+          </>
+        )}
+      </div>
     </div>
   );
 }
